32c709e963ed8c12f1efc3a66c8ded2f99635ba1
arnameti <arnameti@gmail.com>
2020-12-08 18:44:29 +0100

    methode verbessert

--- api/serializers.py
+++ api/serializers.py
@@ -13,21 +13,28 @@ class PlaceSerializer(serializers.ModelSerializer):
         model = Place
         fields = '__all__'
 
+
 # Mit depth = 1 wird eine Ebene tiefer gegangen. D.h in diesem Fall ist beim Aufruf des Prividers, auch die Anschrift ersichtlich.
 class ProviderSerializer(serializers.ModelSerializer):
-    place_pk = serializers.PrimaryKeyRelatedField(queryset=Place.objects.all(), source='place_fk', write_only=True, label='Place')
+    place_pk = serializers.PrimaryKeyRelatedField(queryset=Place.objects.all(), source='place_fk', write_only=True,
+                                                  label='Place')
+
     class Meta:
         model = Provider
         fields = '__all__'
         depth = 1
 
+
 # Der Serializer für die Klasse Teacher.
 class TeacherSerializer(serializers.ModelSerializer):
-    email_pk = serializers.PrimaryKeyRelatedField(queryset=Place.objects.all(), source='email_fk', write_only=True, label='Email')
+    email_pk = serializers.PrimaryKeyRelatedField(queryset=Place.objects.all(), source='email_fk', write_only=True,
+                                                  label='Email')
+
     class Meta:
         model = Teacher
         fields = '__all__'
         depth = 1
+
 
 # Der Serializer für die Klasse Student
 class StudentSerializer(serializers.ModelSerializer):
@@ -36,16 +43,21 @@ class StudentSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 1
 
+
 # Der Serializer für die Klasse Course
 class CourseSerializer(serializers.ModelSerializer):
-    student_pk = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all(), source='course_student', write_only=True, many=True, label='Student')
-    provider_pk = serializers.PrimaryKeyRelatedField(queryset=Provider.objects.all(), source='provider_fk', write_only=True, label='Provider')
-    teacher_pk = serializers.PrimaryKeyRelatedField(queryset=Teacher.objects.all(), source='teacher_fk', write_only=True, label='Teacher')
+    student_pk = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all(), source='course_student',
+                                                    write_only=True, many=True, label='Student')
+    provider_pk = serializers.PrimaryKeyRelatedField(queryset=Provider.objects.all(), source='provider_fk',
+                                                     write_only=True, label='Provider')
+    teacher_pk = serializers.PrimaryKeyRelatedField(queryset=Teacher.objects.all(), source='teacher_fk',
+                                                    write_only=True, label='Teacher')
 
     class Meta:
         model = Course
         fields = '__all__'
         depth = 2
+
 
 # Für diesen Serializer ist kein model erstellt worden. Hier wird von Django der User bereitgestellt, der importiert werden muss.
 # Der User muss sich authentifizieren, um die Kurse, Kursteilnehmer etc. erstellen zu können.
@@ -54,23 +66,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}, 'id': {'read_only': True},
-                         'email': {'required': True},
-                         'first_name': {'required': True}, 'last_name': {'required': True}}
+                        'email': {'required': True},
+                        'first_name': {'required': True}, 'last_name': {'required': True}}
 
-        def create(self, validated_data):
-            password = validated_data.pop('password', None)
-            try:
-                if User.objects.filter(email=validated_data.get('email')).exists():
-                    raise serializers.ValidationError('Email already registered.')
-                password_validation.validate_password(password)
-            except ValidationError as ve:
-                raise serializers.ValidationError({'Password-Errors': [i for i in ve.messages]})
-            user = User(**validated_data)
-            user.set_password(password)
-            user.save()
-            return user
-
-        def update(self, instance, validated_data):
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        try:
             if User.objects.filter(email=validated_data.get('email')).exists():
                 raise serializers.ValidationError('Email already registered.')
-            return super(UserSerializer, self).update(instance, validated_data)
+            password_validation.validate_password(password)
+        except ValidationError as ve:
+            raise serializers.ValidationError({'Password-Errors': [i for i in ve.messages]})
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if User.objects.filter(email=validated_data.get('email')).exists():
+            raise serializers.ValidationError('Email already registered.')
+        return super(UserSerializer, self).update(instance, validated_data)
